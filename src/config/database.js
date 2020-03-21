1feb4a700d265ee0bf5d090d546ff57cde7b3764
Thays Giotto <giotto.thays@gmail.com>
2020-03-21 08:22:29 -0300

    fix: dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,12 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-console.log(process.env.DB_HOST, process.env.DB_USERNAME)
-
 module.exports = {
   dialect: 'mysql',
-  database: 'urwallet',
-  host: 'urwalletdb.czhtjzcprold.us-east-1.rds.amazonaws.com',
-  username: 'admin',
-  password: 'thaysthays',
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   define: {
     underscored: true,
     timestamps: false
