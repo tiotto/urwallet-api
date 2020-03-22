c97a89e7bf2efaa91d3411e3da22d3b8c32045a8
Thays Giotto <giotto.thays@gmail.com>
2020-03-22 18:30:52 -0300

    feat: cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+
 const routes = require('./routes')
 
 require('./database')
 
 const app = express()
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use(express.json())
 
