d4ca750cfecbb95724ae27d3d954e84f850ebcc3
Thays Giotto <giotto.thays@gmail.com>
2020-03-31 17:16:51 -0300

    feat: preflight

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 const express = require('express')
-
+const cors = require('cors')
 const routes = require('./routes')
 
 require('./database')
 
 const app = express()
 
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
+app.options('*', cors())
 
 app.use(express.json())
 
