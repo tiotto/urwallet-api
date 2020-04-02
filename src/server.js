dcbed5c14129795e315772e2ed7e64f354d7ca69
Thays Giotto <giotto.thays@gmail.com>
2020-04-01 23:44:50 -0300

    fix: preflight curl

--- src/server.js
+++ src/server.js
@@ -8,8 +8,9 @@ const app = express()
 
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
   next()
 })
 
@@ -19,4 +20,4 @@ app.use(express.json())
 
 app.use(routes)
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3001)
